95ef0810d825f24e8781cff2ed18d9f3537e8f2d
HarunaYamaguchi <haruna.yamaguchi@rakus-partners.co.jp>
2021-06-21 11:52:19 +0900

    firebaseの設定

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Router from './Router';
+// import './App.css';
+// import { useDispatch, useSelector } from 'react-redux';
+
+const App = () => {
+// const dispatch = useDispatch();
+// const selector = useSelector((state) => state );
+// console.log(selector.users)
+
+  return (
+    <div>
+      <Router />
+    </div>
+  );
+}
+
+export default App;
